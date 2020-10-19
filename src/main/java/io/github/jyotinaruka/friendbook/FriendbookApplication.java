f5473b9e0bda6e66e0ab692982d4e3ad7e1d06ba
Jyoti Singh <jn.naruka@gmail.com>
2020-10-19 13:02:38 -0700

    add spring boot files

---
+++ src/main/java/io/github/jyotinaruka/friendbook/FriendbookApplication.java
@@ -0,0 +1,13 @@
+package io.github.jyotinaruka.friendbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendbookApplication.class, args);
+	}
+
+}
